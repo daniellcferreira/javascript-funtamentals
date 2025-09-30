97e6751f822903f7b3a00a76050b74a34c54d995
Daniel Ferreira <dcfer2017@gmail.com>
2025-09-30 11:45:15 -0300

    feat(desafios): adicionar desafio-1 com variáveis, prompt e validações

--- projeto-js/app.js
+++ projeto-js/app.js
@@ -3,5 +3,7 @@ let numeroSecreto = 5;
 let chute = prompt('Escolhe um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
-  console.log('Isso ai! Você descobriu o numero secreto')
+  alert(`Isso ai! Você descobriu o numero secreto ${numeroSecreto}`)
+} else {
+  alert('Você errou!')
 }
