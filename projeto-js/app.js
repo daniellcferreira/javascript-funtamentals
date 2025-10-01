0d6b1bdc26096d776032b425256675dd0778a26d
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-01 10:04:14 -0300

    feat(app): adicionar contagem de tentativas e dicas de maior/menor

--- projeto-js/app.js
+++ projeto-js/app.js
@@ -1,9 +1,20 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-let chute = prompt('Escolhe um número entre 1 e 10');
+let chute;
+let tentativas = 1;
 
-if (chute == numeroSecreto) {
-  alert(`Isso ai! Você descobriu o numero secreto ${numeroSecreto}`)
-} else {
-  alert('Você errou!')
+while (chute != numeroSecreto) {
+  chute = prompt('Escolhe um número entre 1 e 10');
+
+  if (chute == numeroSecreto) {
+  alert(`Isso ai! Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}`);
+    } else {
+      alert(`O número secreto é maior que ${chute}`);
+    }
+
+    tentativas++;
+  }
 }
