f9f6ca14eb099049b92d219dd13d72c127b838ab
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-03 13:17:34 -0300

    feat: adicionar funções para exibir textos dinâmicos e verificar chute no jogo

--- js-example-2/app.js
+++ js-example-2/app.js
@@ -1,5 +1,24 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Digite um número entre 1 e 10';
 
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTexto(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+exibirTexto('h1', 'Jogo do número secreto');
+exibirTexto('p', 'Digite um número entre 1 e 10');
+
+function verificarChute() {
+  let chute = document.querySelector('input').value;
+  console.log(numeroSecreto == chute);
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10) + 1;
+}
