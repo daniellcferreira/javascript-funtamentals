d2165ed232a42e1829c2d600c92a2f44f9583fd8
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-04 14:43:26 -0300

    feat: implementar lógica completa do jogo do número secreto com tentativas, feedback e reinício

--- js-example-2/app.js
+++ js-example-2/app.js
@@ -5,20 +5,53 @@
 // paragrafo.innerHTML = 'Digite um número entre 1 e 10';
 
 let numeroSecreto = gerarNumeroAleatorio();
+let tantativas = 1;
 
 function exibirTexto(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
 }
 
-exibirTexto('h1', 'Jogo do número secreto');
-exibirTexto('p', 'Digite um número entre 1 e 10');
+function exibirMensagemInicial() {
+  exibirTexto('h1', 'Jogo do número secreto');
+  exibirTexto('p', 'Digite um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
   let chute = document.querySelector('input').value;
-  console.log(numeroSecreto == chute);
+  
+  if (chute == numeroSecreto) {
+    exibirTexto('h1', 'Você acertou!');
+    let palavraTentativas = tantativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativas = `Parabéns! você acertou o número secreto em ${tantativas} ${palavraTentativas}!`;
+    exibirTexto('p', mensagemTentativas);
+    document.getElementById('reiniciar').removeAttribute('disabled');
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTexto('p', 'O número secreto é menor');
+    } else {
+      exibirTexto('p', 'O número secreto é maior');
+    }
+    tantativas++;
+    limparCampo();
+  }
 }
 
 function gerarNumeroAleatorio() {
   return parseInt(Math.random() * 10) + 1;
+}
+
+function limparCampo() {
+  chute = document.querySelector('input');
+  chute.value = '';
+}
+
+function reiniciarJogo() {
+  numeroSecreto = gerarNumeroAleatorio();
+  limparCampo();
+  tantativas = 1;
+  exibirMensagemInicial();
+  document.getElementById('reiniciar').setAttribute('disabled', 'true');
 }
