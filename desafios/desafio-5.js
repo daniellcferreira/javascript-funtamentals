78feb5e98505aa97c8ed73f4b228035c35ef20ce
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-06 12:20:18 -0300

    feat: adicionar listas de linguagens e exemplos de acesso a elementos no desafio-5.js

---
+++ desafios/desafio-5.js
@@ -0,0 +1,22 @@
+// 1- Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// 2- Crie uma lista de linguagens de programação chamada linguagensDeProgramacao.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// 3- Adicione à lista linguagensDeProgramacao os seguintes elementos: Java, Ruby e GoLang.
+let linguagensDeProgramacao1 = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+// Lista ao final: ['JavaScript', 'C', 'C++', 'Kotlin', 'Python', 'Java', 'Ruby', 'GoLang']
+
+// 4- Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[0]);
+
+// 5- Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[1]);
+
+// 6- Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[2]);
