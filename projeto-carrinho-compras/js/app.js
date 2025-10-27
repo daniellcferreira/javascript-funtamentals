df64ed2ea04a2af8105b9b0db2f767d66eba4cfc
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-27 09:58:35 -0300

    feat(js): calcular valor total e exibir detalhes do produto adicionado

--- projeto-carrinho-compras/js/app.js
+++ projeto-carrinho-compras/js/app.js
@@ -1,6 +1,14 @@
 function adicionar() {
-  let nomeProduto = document.getElementById("produto").value;
-  let quantidade = document.getElementById("quantidade").value;
+  let produto = document.getElementById("produto").value;
+  let nomeProduto = produto.split("-")[0];
+  let valorUnitario = produto.split("R$")[1];
 
-  alert("Produto: " + nomeProduto + "\nQuantidade: " + quantidade);
+  let quantidade = document.getElementById("quantidade").value;
+  let preco = quantidade * valorUnitario;
+
+  alert(`O produto ${nomeProduto} com valor unitário de R$${valorUnitario} adicionado com sucesso! O preço total é R$${preco.toFixed(2)}.`);
+
+  
+
+
 }
