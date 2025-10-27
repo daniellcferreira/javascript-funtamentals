4cab6f4dccad7fe74fca044ce0fcee36c6ed13bc
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-27 09:48:32 -0300

    feat(js): implementar função inicial de adicionar produto ao carrinho

---
+++ projeto-carrinho-compras/js/app.js
@@ -0,0 +1,6 @@
+function adicionar() {
+  let nomeProduto = document.getElementById("produto").value;
+  let quantidade = document.getElementById("quantidade").value;
+
+  alert("Produto: " + nomeProduto + "\nQuantidade: " + quantidade);
+}
