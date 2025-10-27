7bac07a52f648f13e64cd027206afbac051ab6c0
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-27 10:09:17 -0300

    feat(js): exibir produtos adicionados dinamicamente no carrinho

--- projeto-carrinho-compras/js/app.js
+++ projeto-carrinho-compras/js/app.js
@@ -2,13 +2,12 @@ function adicionar() {
   let produto = document.getElementById("produto").value;
   let nomeProduto = produto.split("-")[0];
   let valorUnitario = produto.split("R$")[1];
-
   let quantidade = document.getElementById("quantidade").value;
+
   let preco = quantidade * valorUnitario;
 
-  alert(`O produto ${nomeProduto} com valor unitário de R$${valorUnitario} adicionado com sucesso! O preço total é R$${preco.toFixed(2)}.`);
-
-  
-
-
+  let carrinho = document.getElementById("lista-produtos");
+  carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
 }
