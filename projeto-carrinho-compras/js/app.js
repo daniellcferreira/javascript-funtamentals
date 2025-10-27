7302a4c4bdfa607a14d54f1d74fd02b23abf2b6d
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-27 10:21:03 -0300

    feat(js): adicionar cálculo do total geral e função de limpeza do carrinho

--- projeto-carrinho-compras/js/app.js
+++ projeto-carrinho-compras/js/app.js
@@ -1,3 +1,6 @@
+let totalGeral;
+limpar();
+
 function adicionar() {
   let produto = document.getElementById("produto").value;
   let nomeProduto = produto.split("-")[0];
@@ -7,7 +10,21 @@ function adicionar() {
   let preco = quantidade * valorUnitario;
 
   let carrinho = document.getElementById("lista-produtos");
-  carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+  carrinho.innerHTML =
+    carrinho.innerHTML +
+    `<section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
         </section>`;
+
+  totalGeral += preco;
+  let campoTotal = document.getElementById("valor-total");
+  campoTotal.textContent = `R$ ${totalGeral}`;
+
+  document.getElementById("quantidade").value = 0;
+}
+
+function limpar() {
+  totalGeral = 0;
+  document.getElementById("lista-produtos").innerHTML = "";
+  document.getElementById("valor-total").textContent = "R$ 0";
 }
