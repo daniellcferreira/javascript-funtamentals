7d6ad145b927669b04140f4461f9f82f0e3eeb96
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-28 10:53:30 -0300

    feat: adicionar lógica de compra para todos os tipos de ingresso

--- projeto-ingresso/js/app.js
+++ projeto-ingresso/js/app.js
@@ -4,8 +4,11 @@ function comprar() {
 
   if (tipo == "pista") {
     comprarPista(qtd);
+  } else if (tipo == "superior") {
+    comprarSuperior(qtd);
+  } else {
+    comprarInferior(qtd);
   }
-
 }
 
 function comprarPista(qtd) {
@@ -16,6 +19,34 @@ function comprarPista(qtd) {
   } else {
     qtdPista -= qtd;
     document.getElementById("qtd-pista").textContent = qtdPista;
-    alert("Compra realizada com sucesso!");   
+    alert("Compra realizada com sucesso!");
+  }
+}
+
+function comprarSuperior(qtd) {
+  let qtdSuperior = parseInt(
+    document.getElementById("qtd-superior").textContent
+  );
+
+  if (qtd > qtdSuperior) {
+    alert("Quantidade indisponível para o tipo Superior.");
+  } else {
+    qtdSuperior -= qtd;
+    document.getElementById("qtd-superior").textContent = qtdSuperior;
+    alert("Compra realizada com sucesso!");
+  }
+}
+
+function comprarInferior(qtd) {
+  let qtdInferior = parseInt(
+    document.getElementById("qtd-inferior").textContent
+  );
+
+  if (qtd > qtdInferior) {
+    alert("Quantidade indisponível para o tipo Inferior.");
+  } else {
+    qtdInferior -= qtd;
+    document.getElementById("qtd-inferior").textContent = qtdInferior;
+    alert("Compra realizada com sucesso!");
   }
 }
