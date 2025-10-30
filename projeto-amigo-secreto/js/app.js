832e2789b5dad220987efc8bb26fc717b2908068
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-30 10:57:10 -0300

    feat(js): implementar lógica de sorteio e gerenciamento de amigos no jogo Amigo Secreto

--- projeto-amigo-secreto/js/app.js
+++ projeto-amigo-secreto/js/app.js
@@ -14,10 +14,19 @@ function adicionar() {
 }
 
 function sortear() {
-  embaralha(amigos);
+  embaralhar(amigos);
+  let sorteio = document.getElementById("lista-sorteio");
+
+  for (let i = 0; i < amigos.length; i++) {
+    if (i == amigos.length - 1) {
+      sorteio.innerHTML += amigos[i] + " --> " + amigos[0] + "<br>";
+    } else {
+      sorteio.innerHTML += amigos[i] + " --> " + amigos[i + 1] + "<br>";
+    }
+  }
 }
 
-function embaralha(lista) {
+function embaralhar(lista) {
   for (let i = lista.length; i; i--) {
     const indiceAleatorio = Math.floor(Math.random() * i);
     [lista[i - 1], lista[indiceAleatorio]] = [
